5beda1c61e09300830c0b58202e008aa490b8502
elDin-Dev <alexmaildese-dev@yahoo.com>
2021-06-17 08:23:00 +0200

    type number

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("code here...");
+function add(n1, n2) {
+    console.log(typeof n1);
+    return n1 + n2;
+}
+var number = 5;
+var number2 = 2.8;
+console.log(add(number, number2));
--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log("code here...")
+function add(n1: number, n2: number) {
+    console.log(typeof n1)
+    return n1 + n2;
+}
+
+const number = 5;
+const number2 = 2.8
+
+console.log(add(number,number2));
