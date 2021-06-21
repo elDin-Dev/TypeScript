50147ea3f1347e8149f785e9072a500ca810b4a7
elDin-Dev <alexmaildese-dev@yahoo.com>
2021-06-21 23:53:50 +0200

    function & types

--- app.js
+++ app.js
@@ -4,4 +4,15 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result' + num);
 }
-console.log(printResult(add(5, 12)));
+printResult(add(5, 12));
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+var combineValues;
+combineValues = add;
+//combineValues=printResult;
+//console.log(combineValues(3,58));
+addAndHandle(10, 10, function (result) {
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -8,3 +8,21 @@ function printResult (num: number):void {
 }
 
 printResult(add(5,12));
+
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void)  {
+    const result = n1 + n2;
+    cb(result);
+}
+
+
+
+let combineValues: (a: number, b:number) => number;
+
+combineValues=add;
+//combineValues=printResult;
+
+//console.log(combineValues(3,58));
+
+addAndHandle(10, 10, (result) => {
+    console.log(result);
+})
