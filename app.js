cb233f97364570a70adc9f85d57a25eb0b17ee08
elDin-Dev <alexmaildese-dev@yahoo.com>
2021-06-21 08:26:26 +0200

    void & types

--- app.js
+++ app.js
@@ -1,29 +1,7 @@
-var AsTypes;
-(function (AsTypes) {
-    AsTypes[AsTypes["AS_NUMBER"] = 5] = "AS_NUMBER";
-    AsTypes[AsTypes["AS_STRING"] = 10] = "AS_STRING";
-})(AsTypes || (AsTypes = {}));
-;
-var ROLE;
-(function (ROLE) {
-    ROLE[ROLE["DEV"] = 3] = "DEV";
-    ROLE["READ_ONLY"] = "a";
-    ROLE[ROLE["AUTHOR"] = 9] = "AUTHOR";
-})(ROLE || (ROLE = {}));
-function combine(input1, input2, type) {
-    //yuou can specify as many types as you want using | 
-    var result;
-    if ((typeof input1 === 'number') && (typeof input2 === 'number')) {
-        result = input1 + input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-console.log(combine(5, 8, 'as-numbers'));
-console.log(combine("a", 8, 'as-strings'));
-function testType(arg) {
-    console.log(arg);
+function printResult(num) {
+    console.log('Result' + num);
 }
-console.log(testType(AsTypes.AS_NUMBER));
+console.log(printResult(add(5, 12)));
--- app.ts
+++ app.ts
@@ -1,29 +1,10 @@
-enum AsTypes {AS_NUMBER = 5, AS_STRING=10};
-enum ROLE {DEV=3, READ_ONLY='a', AUTHOR=9}
-
-type Combinable = number | string;
-type CombinableDescriptor = 'as-numbers' | 'as-strings';
-
-function combine(input1: Combinable, input2: Combinable, type: CombinableDescriptor ) {
-    //yuou can specify as many types as you want using | 
-    let result:number | string;
-    if ( (typeof input1==='number') && (typeof input2==='number'))
-    {
-        result= input1 + input2
-    } else {
-        result= input1.toString() + input2.toString();
-    }
-    
-    return result;
+function add (n1: number, n2: number) {
+    return n1+n2;
 }
 
-console.log(combine(5,8,'as-numbers'));
-console.log(combine("a",8,'as-strings'));
 
-
-function testType (arg: AsTypes)
-{
-    console.log(arg);
+function printResult (num: number):void {
+    console.log('Result' + num);
 }
 
-console.log(testType(AsTypes.AS_NUMBER));
+printResult(add(5,12));
