1bb777713240bfce7a710860038e678b889f09fb
elDin-Dev <alexmaildese-dev@yahoo.com>
2021-06-21 08:14:35 +0200

    union types

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
-//Explicit object type creation
-/*const person: {
-    name: string;
-    age: number
-} = {
-    name: 'Alex',
-    age: 45
-    
-}*/
-//leave typeScript infer the types.
+var AsTypes;
+(function (AsTypes) {
+    AsTypes[AsTypes["AS_NUMBER"] = 5] = "AS_NUMBER";
+    AsTypes[AsTypes["AS_STRING"] = 10] = "AS_STRING";
+})(AsTypes || (AsTypes = {}));
+;
 var ROLE;
 (function (ROLE) {
     ROLE[ROLE["DEV"] = 3] = "DEV";
     ROLE["READ_ONLY"] = "a";
     ROLE[ROLE["AUTHOR"] = 9] = "AUTHOR";
 })(ROLE || (ROLE = {}));
-var person = {
-    name: 'Alex',
-    age: 45,
-    hobbies: ['Sports', 'Cooking'],
-    //role: [2,'author']
-    role: ROLE.READ_ONLY
-};
-//person.role.push('admin');
-person.role[1] = 10;
-console.log(person);
-var favouriteHobbies;
-favouriteHobbies = ['a'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var h = _a[_i];
-    console.log(h.toLocaleUpperCase());
+function combine(input1, input2, type) {
+    //yuou can specify as many types as you want using | 
+    var result;
+    if ((typeof input1 === 'number') && (typeof input2 === 'number')) {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
+console.log(combine(5, 8, 'as-numbers'));
+console.log(combine("a", 8, 'as-strings'));
+function testType(arg) {
+    console.log(arg);
+}
+console.log(testType(AsTypes.AS_NUMBER));
--- app.ts
+++ app.ts
@@ -1,4 +1,7 @@
-function combine(input1: number | string, input2: number | string) {
+enum AsTypes {AS_NUMBER = 5, AS_STRING=10};
+enum ROLE {DEV=3, READ_ONLY='a', AUTHOR=9}
+
+function combine(input1: number | string, input2: number | string, type: 'as-numbers' | 'as-strings') {
     //yuou can specify as many types as you want using | 
     let result:number | string;
     if ( (typeof input1==='number') && (typeof input2==='number'))
@@ -11,5 +14,13 @@ function combine(input1: number | string, input2: number | string) {
     return result;
 }
 
-console.log(combine(5,8));
-console.log(combine("5",8));
+console.log(combine(5,8,'as-numbers'));
+console.log(combine("a",8,'as-strings'));
+
+
+function testType (arg: AsTypes)
+{
+    console.log(arg);
+}
+
+console.log(testType(AsTypes.AS_NUMBER));
