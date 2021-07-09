b1ba6fdd87b32f99d3a870d6b3fd433e4472159c
elDin-Dev <alexmaildese-dev@yahoo.com>
2021-07-09 16:20:13 +0200

    type guards

--- dist/app.js
+++ dist/app.js
@@ -3,3 +3,54 @@ var button = document.querySelector("button");
 button.addEventListener('click', function () {
     console.log("CLICKED!");
 });
+var e1 = {
+    name: 'Alex',
+    privileges: ['create-server'],
+    startDate: new Date(),
+};
+function add(a, b) {
+    if ((typeof a === 'string') || (typeof b === 'string')) {
+        return a.toString();
+        +b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log('Name:' + emp.name);
+    if ('privileges' in emp) {
+        console.log('Name:' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('startDate:' + emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+var Car = (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log('driving a car');
+    };
+    return Car;
+}());
+var Truck = (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log('driving a truck');
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log('loading cargo: ' + amount);
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(v) {
+    v.drive();
+    if (v instanceof Truck) {
+        v.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,71 @@ type Employee = {
 
 type ElevatedEmpployee = Admin & Employee;
 
+
 const e1: ElevatedEmpployee = {
+
     name: 'Alex',
+    privileges:['create-server'],
+    startDate: new Date(),
+
 }
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+    if ( (typeof a==='string') || (typeof b==='string') ) {
+        return a.toString(); + b.toString();
+    }
+
+    return a+b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee)
+{
+    console.log('Name:' + emp.name);
+    if ('privileges' in emp) {
+        console.log('Name:' + emp.privileges);
+    }
+
+    if ('startDate' in emp) {
+        console.log('startDate:' + emp.startDate);
+    }
+
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+    drive(){
+        console.log('driving a car');
+    }
+}
+
+class Truck {
+    drive(){
+        console.log('driving a truck');
+    }
+
+    loadCargo(amount: number){
+        console.log('loading cargo: '+amount);
+    }
+}
+
+type Vehicle= Car | Truck;
+const v1= new Car();
+const v2= new Truck();
+
+function useVehicle(v: Vehicle) {
+    v.drive();
+    //it is possible to use instanveof cuase class is transpiled to
+    //constructor which vanila js knows. is not possible to use wiht interfaces.
+    if (v instanceof Truck) {
+        v.loadCargo(1000);
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
