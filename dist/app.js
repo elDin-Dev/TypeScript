c2f0cc25cbfffe41d2cf2cea15e4657230b94112
elDin-Dev <alexmaildese-dev@yahoo.com>
2021-07-09 13:36:01 +0200

    implementing interfaces

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,13 @@
 "use strict";
-let user1;
-user1 = {
-    name: 'Alex',
-    age: 45,
-    greet(phrase) {
-        console.log(phrase + ' ' + this.name);
+class Person {
+    constructor(n) {
+        this.age = 45;
+        this.name = n;
     }
-};
+    greet(phrase) {
+        console.log(phrase + ' ' + this.name + ' age ' + this.age);
+    }
+}
+let user1;
+user1 = new Person('Alex');
 user1.greet("Hi there. I'm ");
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,23 @@
-interface Person {
+interface Greetable {
     name: string,
-    age:number,
 
     greet(phrase: string):void;
 }
 
-let user1: Person;
-
-user1 = {
-
-    name : 'Alex',
-    age: 45,
-    greet(phrase: string) {
-        console.log(phrase + ' ' + this.name);
+class Person implements Greetable {
+    name: string;
+    age:number= 45;
+    constructor(n: string) {
+        this.name=n;
     }
-};
+    greet(phrase: string): void {
+        console.log(phrase + ' ' + this.name + ' age ' +this.age);
+    }
+    
+}
+
+let user1: Greetable;
+
+user1 = new Person('Alex')
 
 user1.greet("Hi there. I'm ");
