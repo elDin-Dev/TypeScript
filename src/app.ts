58e2c79f491a26392dcd1c037b6458ce51295ae6
elDin-Dev <alexmaildese-dev@yahoo.com>
2021-07-09 15:50:07 +0200

    interceptor types

--- src/app.ts
+++ src/app.ts
@@ -5,3 +5,18 @@ button.addEventListener('click', () => {
 });
 
 
+type Admin = {
+    name: string;
+    privileges: string[];
+}
+
+type Employee = {
+    name: string;
+    startDate: Date;
+}
+
+type ElevatedEmpployee = Admin & Employee;
+
+const e1: ElevatedEmpployee = {
+    name: 'Alex',
+}
