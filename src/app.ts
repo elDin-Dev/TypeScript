7d08887a256c615d03e22224d6b9f6adea624fbc
elDin-Dev <alexmaildese-dev@yahoo.com>
2021-07-12 16:22:24 +0200

    first decorator

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,18 @@
-const button = document.querySelector("button")!;
+function Logger(constructor: Function) {
+    console.log('logging .... ');
+    console.log(constructor);
+}
 
-button.addEventListener('click', () => {
-    console.log("CLICKED!");
-});
+@Logger
+class Person {
+    name= 'Max';
 
+    constructor() {
+        console.log('Creating a person');
+    }
 
+}
+
+const pers = new Person();
+
+console.log(pers);
